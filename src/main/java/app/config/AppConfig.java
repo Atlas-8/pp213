500bc6d36b6b069d746254eabc3b8c46d7d22788
Atlas-8 <lincherabit@gmail.com>
2020-06-14 10:37:24 +0300

    Initial commit

---
+++ src/main/java/app/config/AppConfig.java
@@ -0,0 +1,24 @@
+package app.config;
+
+import app.model.Animal;
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "app")
+public class AppConfig {
+
+    @Bean(name="Dog")
+    public Animal Dog() {
+        return new Dog();
+    }
+
+    @Bean(name="Timer")
+    public Timer timer() {
+        return new Timer();
+    }
+
+}
